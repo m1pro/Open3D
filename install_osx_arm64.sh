ada051f7686967d0766f8787da0f4394bb1e9119
user <user@users-MBP.lan>
2021-03-20 17:01:42 +0200

    mend

--- install_osx_arm64.sh
+++ install_osx_arm64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # activate the desired conda env and run from root open3d folder 
-set -e
+set -x
 
 echo "installing pre-requisited"
 sudo port install openblas
@@ -9,6 +9,7 @@ sudo chown root:admin /opt/local/include/cblas.h
 brew install libpng
 
 echo "build open3d and install to the current conda env"
+mkdir -p build
 pushd build
 cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX ..
 make install-pip-package -j8 
